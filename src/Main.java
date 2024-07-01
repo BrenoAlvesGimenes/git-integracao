94ea8d5bb109b1bd5e60b114a8221119c0df16c2
BrenoAlvesGimenes <walvesgimenes@gmail.com>
2024-07-01 16:36:20 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,5 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Teste");
-        System.out.println("Teste 2");
-        System.out.println("Teste 3");
-        System.out.println("Teste 4");
     }
 }
