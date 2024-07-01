dd05ee2a4d3d15011d978a8a5fc7a5252753b332
BrenoAlvesGimenes <walvesgimenes@gmail.com>
2024-07-01 16:35:08 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Teste");
         System.out.println("Teste 2");
+        System.out.println("Teste 3");
     }
 }
